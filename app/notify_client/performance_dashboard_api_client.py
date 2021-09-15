fb7b3d935ebbd8f1da9e8a2f42c40e6f38205bb8
Kevin BALOG <kevinbalog21@gmail.com>
2021-09-15 19:36:38 +0200

    Rename to perfomance dashboard

--- app/notify_client/performance_dashboard_api_client.py
+++ app/notify_client/performance_dashboard_api_client.py
@@ -1,10 +1,10 @@
 from app.notify_client import NotifyAdminAPIClient
 
 
-class PerformancePlatformAPIClient(NotifyAdminAPIClient):
+class PerformanceDashboardAPIClient(NotifyAdminAPIClient):
 
-    def get_performance_platform_stats(self, params_dict=None):
-        return self.get("/performance-platform", params=params_dict)
+    def get_performance_dashboard_stats(self, params_dict=None):
+        return self.get("/performance-dashboard", params=params_dict)
 
 
-performance_platform_api_client = PerformancePlatformAPIClient()
+performance_dashboard_api_client = PerformanceDashboardAPIClient()
