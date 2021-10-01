76d72cba2ebf13abb85119820cf57f2c30a84ff5
Kevin <kevinbalog21@gmail.com>
2021-10-01 16:02:52 +0200

    Add support for single quote in SMS sender name

--- app/main/forms.py
+++ app/main/forms.py
@@ -44,7 +44,7 @@ from app.main.validators import (
     CommonlyUsedPassword,
     CsvFileValidator,
     DoesNotStartWithDoubleZero,
-    LettersNumbersFullStopsAndUnderscoresOnly,
+    LettersNumbersSingleQuotesFullStopsAndUnderscoresOnly,
     MustContainAlphanumericCharacters,
     NoCommasInPlaceHolders,
     NoEmbeddedImagesInSVG,
@@ -1648,7 +1648,7 @@ class ServiceSmsSenderForm(StripWhitespaceForm):
             DataRequired(message="Cannot be empty"),
             Length(max=11, message="Enter 11 characters or fewer"),
             Length(min=3, message="Enter 3 characters or more"),
-            LettersNumbersFullStopsAndUnderscoresOnly(),
+            LettersNumbersSingleQuotesFullStopsAndUnderscoresOnly(),
             DoesNotStartWithDoubleZero(),
             SenderBlocklistValidator(values=[])
         ]
